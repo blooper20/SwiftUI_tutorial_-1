76071a6ed32ca023d83ebcd239c49786be7769a2
blooper20 <yjw127588@gmail.com>
2022-05-13 18:09:55 +0900

    Initial Commit

---
+++ SwiftUI_tutorial_#1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial_#1
+//
+//  Created by MacBook Air on 2022/05/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_tutorial_#1/SwiftUI_tutorial__1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorial__1App.swift
+//  SwiftUI_tutorial_#1
+//
+//  Created by MacBook Air on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorial__1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
